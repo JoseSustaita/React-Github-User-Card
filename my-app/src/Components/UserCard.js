1f8d7e239a139512db55a83af8a3c11dae6587ce
Jose Sustaita <josesustaita70@gmail.com>
2020-04-13 18:51:48 -0400

    Got my own user data to show up from api request

--- my-app/src/Components/UserCard.js
+++ my-app/src/Components/UserCard.js
@@ -1,23 +1,21 @@
 import React from "react";
-import "./App.css";
-import UserCard from "./Components/UserCard";
 
-class App extends React.Component {
+class UserCard extends React.Component {
   constructor() {
     super();
     this.state = {
-      followers: [],
-      followersText: "",
+      avatar: [],
+      location: [],
     };
   }
   componentDidMount() {
     console.log("Component did mount");
     fetch("https://api.github.com/users/josesustaita")
       .then((res) => res.json())
-      .then((followers) => {
-        console.log(followers);
-        this.setState({ avatar: followers.avatar_url });
-        this.setState({ location: followers.location });
+      .then((user) => {
+        console.log(user);
+        this.setState({ avatar: user.avatar_url });
+        this.setState({ location: user.location });
       })
       .catch((err) => console.error(err));
   }
@@ -27,10 +25,12 @@ class App extends React.Component {
     return (
       <div>
         <div className="Usercard">
-          <UserCard avatar={this.state.avatar} location={this.state.location} />
+          <h1>Hello I'm Jose!</h1>
+          <img src={this.props.avatar} alt="User Avatar" />
+          <h2>{this.props.location}</h2>
         </div>
       </div>
     );
   }
 }
-export default App;
+export default UserCard;
